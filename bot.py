28d50828133352f018958ba9769c5145c17f9b95
lucian-xinitrc <cusmirlucian@gmail.com>
2026-02-20 23:07:05 +0000

    New commit

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = commands.Bot(
 
 last_cache = None
 
-def decrypt(self, ciphertext_b64):
+def decrypt(ciphertext_b64):
 	    cipher = AES.new(os.getenv('encryption_key').encode(), AES.MODE_ECB)
 	    decrypted = cipher.decrypt(base64.b64decode(ciphertext_b64))
 	    pad = decrypted[-1]
