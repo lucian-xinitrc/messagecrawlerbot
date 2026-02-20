e1dcfe05e6f956a3c2c0e0c84196385c5a3f43ad
lucian-xinitrc <cusmirlucian@gmail.com>
2026-02-20 22:38:58 +0000

    First commit

---
+++ bot.py
@@ -0,0 +1,58 @@
+import os, disnake, requests, psycopg2, base64
+from disnake.ext import tasks
+from dotenv import load_dotenv
+from disnake.ext import commands
+
+load_dotenv()
+
+bot = commands.Bot(
+	intents=disnake.Intents.all(), 
+	allowed_mentions=disnake.AllowedMentions(everyone=True)
+)
+
+last_cache = None
+
+def decrypt(self, ciphertext_b64):
+	    cipher = AES.new(os.getenv('encryption_key').encode(), AES.MODE_ECB)
+	    decrypted = cipher.decrypt(base64.b64decode(ciphertext_b64))
+	    pad = decrypted[-1]
+	    return decrypted[:-pad].decode('utf-8')
+
+@tasks.loop(seconds=2)
+async def watcher():
+	global last_cache
+	conn = psycopg2.connect(db)
+	cur = conn.cursor()
+
+	cur.execute("SELECT author, message FROM public.last_message WHERE id = 1")
+	row = cur.fetchone()
+
+	cur.close()
+	conn.close()
+	if not row:
+	    return
+
+	if row != last_cache:
+	    last_cache = row
+
+	    author, msg = row
+	    channel = bot.get_channel(1473044902492246219)
+	    decryptedMsg = config.Config().decrypt(msg)
+	    check = True
+	    if channel:
+	        for word in ["login", "register", "msg", "/give"]:
+	        	if word in decryptedMsg:
+	        		check = False
+	        		break
+	        if check:
+	        	await channel.send(f"**{config.Config().decrypt(author)}**: {config.Config().decrypt(msg)}")
+
+watcher.start()
+
+@bot.event
+async def on_ready():
+	activity = disnake.Game(name="Scanning for messages...")
+	await bot.change_presence(status=disnake.Status.idle, activity=activity)
+
+if __name__ == "__main__":
+	bot.run(os.getenv('bot_token'))
